2e5d7c792101240e38870e34db41075944f9ae44
fedepenic <fedepenic@hotmail.com>
2023-08-01 21:05:21 -0300

    Create BikeRental Domain Class.

---
+++ fiubike/grails-app/domain/fiubike/BikeRental.groovy
@@ -0,0 +1,16 @@
+package fiubike
+
+class BikeRental {
+
+    int id;
+    String creationTimestamp;
+    String rentStartTimestamp;
+    String rentEndTimestamp;
+    float totalCost; 
+    int bikeId;
+    int renterId;
+    boolean rentalCompleted;
+
+    static constraints = {
+    }
+}
---
+++ fiubike/src/test/groovy/fiubike/BikeRentalSpec.groovy
@@ -0,0 +1,18 @@
+package fiubike
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class BikeRentalSpec extends Specification implements DomainUnitTest<BikeRental> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
